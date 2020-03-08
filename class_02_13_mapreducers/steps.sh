35fe40fb1897200d7e7027d27d038c52ed52bd5c
Bruno Luz <bruno.al.luz@gmail.com>
2020-03-08 09:46:18 -0300

    exercices done

--- class_02_13_mapreducers/steps.sh
+++ class_02_13_mapreducers/steps.sh
@@ -6,5 +6,6 @@ yum install python-pip -y
 # run script locally, without hadoop
 python RatingsBreakdown.py u.data
 
-# run within hadoop
+# run within hadoop (inside hadoop cluster)
 python RatingsBreakdown.py -r hadoop --hadoop-streaming-jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar u.data
+python RatingsBreakdown.py -r hadoop --hadoop-streaming-jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar hdfs:///user/maria_dev/u.data
