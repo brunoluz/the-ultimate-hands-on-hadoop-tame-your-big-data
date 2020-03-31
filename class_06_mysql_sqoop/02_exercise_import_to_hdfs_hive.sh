dc20e1b44b4f4d2f52ccebd4a75ac670ca156028
Bruno Luz <bruno.al.luz@gmail.com>
2020-03-30 22:29:37 -0300

    sqoop exercises

--- class_06_mysql_sqoop/02_exercise_import_to_hdfs_hive.sh
+++ class_06_mysql_sqoop/02_exercise_import_to_hdfs_hive.sh
@@ -17,4 +17,7 @@ vim hive-site.xml
 useSSL=false
 
 sqoop import --connect jdbc:mysql://localhost/movielens --driver com.mysql.jdbc.Driver --table movies -m 1 --username sqimport --password hadoop
+# movies file was created at /user/maria_dev/movies
+hdfs dfs -rm -r /user/maria_dev/movies
+
 sqoop import --connect jdbc:mysql://localhost/movielens --driver com.mysql.jdbc.Driver --table movies -m 1 --username sqimport --password hadoop --hive-import
